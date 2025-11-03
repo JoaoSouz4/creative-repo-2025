57cb8ade54ffd8f9e52d735481f4b6cbd5cee970
JoaoSouz4 <joaosouz8@gmail.com>
2025-11-02 22:01:30 -0200

    .

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -5,19 +5,20 @@ export function Footer(){
     return (
         <footer className = 'dark:bg-neutral-950 mt-8 w-full bg-teal-950 px-8 py-8 flex justify-center'>
             <div className = 'w-[700px]'>
-                <div className = 'flex justify-between items-start'>
-                    <div className = 'relative text-white/90 text-2xl'>
-                        <div className = 'font-modak text-sm'>Repositório Criativo</div>
-                        <div className = 'font-dynalight text-sm text-white/90 absolute left-0 top-4 md:top-6'>
+
+                <div className = 'flex md:flex-row justify-between items-start'>
+                    <div className = 'opacity-[60%] relative text-white/90 text-2xl'>
+                        <div className = 'font-modak text-xs md:text-sm'>Repositório Criativo</div>
+                        <div className = 'font-dynalight text-xs md:text-sm text-white/90 absolute left-0 top-4 md:top-6'>
                             By: João Souza
                         </div>
-                        <div className = 'text-xs text-white/50 mt-4 md:text-sm'>
+                        <div className = 'text-xs text-white/30 mt-4 md:text-sm'>
                             Out 2025
                         </div>
                     </div>
 
                     <div className = 'text-white'>
-                        <h2 className = 'text-sm md:text-xl font-bold'>Contato</h2>
+                        <h2 className = 'text-xs md:text-xl'>Contato do desenvolvedor</h2>
                         <ul className = 'text-xs md:text-sm'>
                             <li className = 'text-white/50'>Email: joaosouz8@gmail.com</li>
                             <li className = 'text-white/50'>Telefone: 69 99348-5751</li>
